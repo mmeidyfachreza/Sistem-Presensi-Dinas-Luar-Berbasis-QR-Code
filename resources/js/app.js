a2c79ab60af72a32a5350cfa77dc3c506532fb36
mmeidyfachreza <meidyfachreza@gmail.com>
2022-06-03 11:29:58 +0800

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,8 +18,8 @@ window.Vue = require('vue').default;
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.prototype.$userId = document.querySelector("meta[name='user_id']").getAttribute('content');
+Vue.component('presensi-component', require('./components/PresensiComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +27,23 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import Vue from 'vue';
+import VueGeolocation from 'vue-browser-geolocation';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(VueSweetalert2);
+Vue.use(PiniaVuePlugin);
+Vue.use(VueGeolocation);
+const pinia = createPinia()
+
 const app = new Vue({
     el: '#app',
+    pinia,
+});
+
+const qrcode = new Vue({
+    el: '#app',
+    pinia,
 });
