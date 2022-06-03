9a2f49e4953bef8338e283a3f67799d479edb8eb
mmeidyfachreza <meidyfachreza@gmail.com>
2022-06-03 11:28:18 +0800

    add presence route api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PresenceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('Api')->group(function () {
+
+    // Route::post('/qrcode-link', 'QRcodeController@refresh');
+    // Route::post('/qrcode', 'QRcodeController@verif');
+
+    // Route::post('/presensi','presensiController@simpan');
+    // Route::get('/presensi/{id}', 'presensiController@getUpdate');
+    // Route::get('/qrcode-link2/{link}', 'QRcodeController@getQR');
+    // Route::get('/qrcode-status/{link}', 'QRcodeController@startQRCode');
+    Route::get('presensi/{presensi}', [PresenceController::class,'show']);
+    Route::post('presensi/verif-qrcode', [PresenceController::class,'store']);
+
+
 });
