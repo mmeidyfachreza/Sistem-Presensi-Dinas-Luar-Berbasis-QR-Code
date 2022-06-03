aced01e7999084da7f4f0ba5c6a4a4faf0ce2301
mmeidyfachreza <meidyfachreza@gmail.com>
2022-06-03 11:25:18 +0800

    initial setup model

--- app/Models/Presence.php
+++ app/Models/Presence.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class Presence extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'employee_id',
+        'field_work_activity_id',
+        'date',
+        'start_time',
+        'end_time',
+        'start_location',
+        'end_location'
+    ];
+
+    public function scopeHavePresence($query,$employeeId)
+    {
+        return $query-> whereEmployeeId($employeeId)->where('date',Carbon::now()->format('Y-m-d'))->exists();
+    }
+
+    public function scopeGetPresence($query,$employeeId)
+    {
+        return $query-> whereEmployeeId($employeeId)->where('date',Carbon::now()->format('Y-m-d'))->first();
+    }
 }
